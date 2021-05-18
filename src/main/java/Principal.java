99575f4c0a28a4f3913e4537e3cc761a72161224
Heitor Ramos <heitorhfbr@gmail.com>
2021-05-18 11:16:01 -0300

    Criando conexao ao banco

---
+++ src/main/java/Principal.java
@@ -0,0 +1,10 @@
+import configuracao.BD;
+
+import java.sql.SQLException;
+
+public class Principal {
+
+    public static void main(String[] args){
+        BD.conexao();
+    }
+}
---
+++ src/main/java/configuracao/BD.java
@@ -0,0 +1,31 @@
+package configuracao;
+
+
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.logging.Logger;
+
+public class BD {
+
+    public static void conexao(){
+
+        final Logger LOG = Logger.getLogger("BD.class");
+
+        String driver = "mysql";
+        String local = "localhost";
+        String nomeBanco = "meuBanco";
+        String usuario = "usuario";
+        String senha = "senha";
+
+        String url = "jdbc:" + driver + "://" + local + "/" + nomeBanco + "?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC";
+
+        //try resource ao finalizar faz o fechamento da conexao
+        try (Connection conn = DriverManager.getConnection(url, usuario, senha)){
+            LOG.info("CONECTADO AO BANCO");
+        } catch (SQLException throwables) {
+            LOG.info("NAO FOI CONECTADO AO BANCO : " + throwables.getMessage());
+        }
+    }
+}
