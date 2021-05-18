0ca9370e416c0f5f76bcf8cffa6bf6632155a28f
Heitor Ramos <heitorhfbr@gmail.com>
2021-05-18 12:17:15 -0300

    Criando e configurando arquivo properties com os dados de acesso ao banco

--- src/main/java/configuracao/BD.java
+++ src/main/java/configuracao/BD.java
@@ -2,9 +2,12 @@ package configuracao;
 
 
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
 import java.util.logging.Logger;
 
 public class BD {
@@ -13,23 +16,33 @@ public class BD {
 
         final Logger LOG = Logger.getLogger("BD.class");
 
-        String driver = "mysql";
-        String local = "localhost";
-        String nomeBanco = "meuBanco";
-        String usuario = "usuario";
-        String senha = "senha";
+        Connection conn = null;
+        try{
 
-        String url = "jdbc:" + driver + "://" + local + "/" + nomeBanco + "?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC";
+            InputStream input = BD.class.getClassLoader().getResourceAsStream("connections.properties");
+            Properties props = new Properties();
+            props.load(input);
 
-        //try resource ao finalizar faz o fechamento da conexao
-        try (Connection conn = DriverManager.getConnection(url, usuario, senha)){
+            String driver = props.getProperty("jdbc.driver");
+            String local = props.getProperty("db.local");
+            String nomeBanco = props.getProperty("db.local");
+            String usuario = props.getProperty("db.usuario");
+            String senha = props.getProperty("db.senha");
+
+            String url = "jdbc:" + driver + "://" + local + "/" + nomeBanco + "?createDatabaseIfNotExist=true&useTimezone=true&serverTimezone=UTC";
+
+            conn = DriverManager.getConnection(url, usuario, senha);
+
+
             LOG.info("CONECTADO AO BANCO");
-            return  conn;
-        } catch (SQLException throwables) {
-            LOG.info("NAO FOI CONECTADO AO BANCO : " + throwables.getMessage());
+
+        } catch (SQLException | IOException e) {
+            LOG.info("NAO FOI CONECTADO AO BANCO : " + e.getMessage());
+        } finally {
+            LOG.info("DESCONECTADO DO BANCO");
         }
 
-        return  null;
+        return conn;
 
     }
 }
